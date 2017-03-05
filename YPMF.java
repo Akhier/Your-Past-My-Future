72b66582604a570583f397c584fd893a7f6e12e4
Akhier <akherthedragonhearted@gmail.com>
2017-03-05 10:50:45 -0500

    Still setup
    and still following the tutorial

--- YPMF.java
+++ YPMF.java
@@ -1,17 +1,44 @@
 import javax.swing.JFrame;
 import asciiPanel.AsciiPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import screens.Screen;
+import screens.StartScreen;
 
-public class YPMF extends JFrame {
+public class YPMF extends JFrame implements KeyListener{
 	private static final long serialVersionUID = 23015753830142527L;
 
 	private AsciiPanel terminal;
+	private Screen screen;
 
 	public YPMF() {
 		super();
 		terminal = new AsciiPanel();
 		add(terminal);
 		pack();
+		screen = new StartScreen();
+		addKeyListener(this);
+		repaint();
 	}
+
+	@Override
+	public void repaint() {
+		terminal.clear();
+		screen.displayOutput(terminal);
+		super.repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		screen = screen.respondToUserInput(e);
+		repaint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) { }
+
+	@Override
+	public void keyTyped(KeyEvent e) { }
 
 	public static void main(String[] args) {
 		YPMF app = new YPMF();
---
+++ screens/WinScreen.java
@@ -0,0 +1,16 @@
+package screens;
+
+import java.awt.event.KeyEvent;
+import asciiPanel.AsciiPanel;
+
+public class WinScreen implements Screen {
+	@Override
+	public void displayOutput(AsciiPanel terminal) {
+
+	}
+
+	@Override
+	public Screen respondToUserInput(KeyEvent key) {
+		return null;
+	}
+}
