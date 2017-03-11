3f92a4940b98c63253ce3f007c515800819ca2ce
Akhier <akherthedragonhearted@gmail.com>
2017-03-11 12:50:23 -0500

    Fixed error with CreatureAi.hunt

--- ypmf/CreatureAi.java
+++ ypmf/CreatureAi.java
@@ -57,8 +57,10 @@ public class CreatureAi {
 
 	public void hunt(Creature target) {
 		List<Point> points = new Path(creature, target.x, target.y).points();
-		int mx = points.get(0).x - creature.x;
-		int my = points.get(0).y - creature.y;
-		creature.moveBy(mx, my, 0);
+		if(points != null) {
+			int mx = points.get(0).x - creature.x;
+			int my = points.get(0).y - creature.y;
+			creature.moveBy(mx, my, 0);
+		}
 	}
 }
